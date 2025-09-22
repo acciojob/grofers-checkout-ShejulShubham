143bb35034539f0d4859cb871373a4c943ec690f
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-09-22 10:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+const table = document.getElementById("table");
+      const getSumBtn = document.createElement("button");
+      getSumBtn.append("Get Total Price");
+      document.body.appendChild(getSumBtn);
+      const tr = document.createElement("tr");
+      const td = document.createElement("td");
+      td.innerText = "Total Price";
+      tr.appendChild(td);
 
-const getSum = () => {
-//Add your code here
-  
-};
+      const getSum = () => {
+        const priceList = document.querySelectorAll(".price");
+        const td = document.createElement("td");
+        let total = 0;
 
-getSumBtn.addEventListener("click", getSum);
+        priceList.forEach((price) => {
+          total += parseInt(price.textContent, 10);
+        });
+
+        td.innerText = total;
+
+        tr.appendChild(td);
+
+        table.appendChild(tr);
+        //Add your code here
+      };
+
+      getSumBtn.addEventListener("click", getSum);
 
